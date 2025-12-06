976527257792d5b61487013e5fcd9805aec2ed2e
fractuskst <fractuskst@yandex.ru>
2025-12-06 16:28:18 +0300

    resolve Mixed Content error using Vercel proxy

--- src/store/api/outlayApi.ts
+++ src/store/api/outlayApi.ts
@@ -1,37 +1,37 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { DeleteOrCreateRowResponse, Row } from 'src/types';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { DeleteOrCreateRowResponse, Row } from "src/types";
 
 export const outlayApi = createApi({
-  reducerPath: 'outlayApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://185.244.172.108:8081/v1' }),
-  tagTypes: ['Row'],
+  reducerPath: "outlayApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  tagTypes: ["Row"],
   endpoints: (builder) => ({
     getRowList: builder.query<Row[], number>({
       query: (eID) => `/outlay-rows/entity/${eID}/row/list`,
-      providesTags: ['Row'],
+      providesTags: ["Row"],
     }),
-    createRow: builder.mutation<DeleteOrCreateRowResponse, { eID: number; rowData: Omit<Row, 'id' | 'child'> }>({
+    createRow: builder.mutation<DeleteOrCreateRowResponse, { eID: number; rowData: Omit<Row, "id" | "child"> }>({
       query: ({ eID, rowData }) => ({
         url: `/outlay-rows/entity/${eID}/row/create`,
-        method: 'POST',
+        method: "POST",
         body: rowData,
       }),
-      invalidatesTags: ['Row'],
+      invalidatesTags: ["Row"],
     }),
     updateRow: builder.mutation<Row, { eID: number; rID: number; rowData: Partial<Row> }>({
       query: ({ eID, rID, rowData }) => ({
         url: `/outlay-rows/entity/${eID}/row/${rID}/update`,
-        method: 'POST',
+        method: "POST",
         body: rowData,
       }),
-      invalidatesTags: ['Row'],
+      invalidatesTags: ["Row"],
     }),
     deleteRow: builder.mutation<DeleteOrCreateRowResponse, { eID: number; rID: number }>({
       query: ({ eID, rID }) => ({
         url: `/outlay-rows/entity/${eID}/row/${rID}/delete`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
-      invalidatesTags: ['Row'],
+      invalidatesTags: ["Row"],
     }),
   }),
 });
